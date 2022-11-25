0ccb968089f43be1f1514517ea216f91159e0aa0
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-11-25 22:18:03 +0900

    add, fix

--- i2g.cc
+++ i2g.cc
@@ -19,6 +19,10 @@
 #define int int32_t
 #include "../goki_check_cc/lieonn.hh"
 typedef myfloat num_t;
+#include "../goki_check_cc/p0.hh"
+#include "../goki_check_cc/p1.hh"
+#include "../goki_check_cc/decompose.hh"
+#include "../goki_check_cc/catg.hh"
 #include "../goki_check_cc/goki.hh"
 
 using std::cout;
@@ -71,7 +75,7 @@ int main(int argc, const char* argv[]) {
       else if(lp.first + 1 < in.rows() && lp.second + 1 < in.cols() &&
               in(lp.first + 1, lp.second + 1) < half)
         p.emplace_back(make_pair(lp.first + 1, lp.second + 1));
-      else if(lp.first + 1 < in.rows()
+      else if(lp.first + 1 < in.rows() &&
               in(lp.first + 1, lp.second) < half)
         p.emplace_back(make_pair(lp.first + 1, lp.second));
       else if(lp.first + 1 < in.rows() && 0 < lp.second - 1 &&
@@ -79,6 +83,7 @@ int main(int argc, const char* argv[]) {
         p.emplace_back(make_pair(lp.first + 1, lp.second - 1));
       else if(                            0 < lp.second - 1 &&
               in(lp.first, lp.second - 1) < half)
+        p.emplace_back(make_pair(lp.first, lp.second - 1));
       else break;
       auto q(p);
       q.erase(q.end() - 1);
@@ -93,10 +98,28 @@ int main(int argc, const char* argv[]) {
       SimpleMatrix<num_t> out(2, sz);
       out.row(0) = (dft<num_t>(- sz) * dft<num_t>(max(sz, py.size())).subMatrix(0, 0, sz, py.size()) * py.template cast<complex<num_t> >()).template real<num_t>();
       out.row(1) = (dft<num_t>(- sz) * dft<num_t>(max(sz, px.size())).subMatrix(0, 0, sz, px.size()) * px.template cast<complex<num_t> >()).template real<num_t>();
-      savep2or3<num_t>(argv[4], out, true);
+      vector<SimpleMatrix<num_t> > out0;
+      out0.emplace_back(std::move(out));
+      savep2or3<num_t>(argv[4], out0, true);
     }
   } else if(m == '-') {
-    
+    std::vector<SimpleMatrix<num_t> > in0;
+    if(! loadp2or3<num_t>(in0, argv[3])) return - 1;
+    auto in(rgb2d<num_t>(in0));
+    SimpleMatrix<num_t> out(sz, sz);
+    out.O(num_t(int(1)));
+    for(int i = 0; i < in.cols(); i ++) {
+      SimpleVector<num_t> st(2);
+      SimpleVector<num_t> ed(2);
+      st[0] = in(0, i);
+      st[1] = in(1, i);
+      ed[0] = in(0, (i + 1) % in.cols());
+      ed[1] = in(1, (i + 1) % in.cols());
+      drawLine<num_t>(out, st, ed, num_t(int(0)));
+    }
+    std::vector<SimpleMatrix<num_t> > out0;
+    out0.emplace_back(std::move(out));
+    savep2or3<num_t>(argv[4], out0, true);
   }
   return 0;
 }
